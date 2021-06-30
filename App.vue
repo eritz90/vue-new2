6849e0317c394deca2825a3103ffc67348fb4f74
eritz90 <46261768+eritz90@users.noreply.github.com>
2021-06-30 18:13:33 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,95 @@
+<template>
+<div class = "container">
+  <Header title = "Task tracker"/>
+  <Tasks :tasks = "tasks"/>
+  </div>
+</template>
+
+<script lang = "ts">
+import Header from '@/components/Header.vue';
+import Tasks from '@/components/Tasks.vue';
+
+export default ({
+  name: 'App',
+  components: {
+    Header,
+    Tasks,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'March 1st at 2:30pm',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'March 3rd at 1:30pm',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'March 1st at 2:30pm',
+        reminder: false,
+      },
+    ];
+  },
+});
+
+</script>
+<style lang="scss">
+#app {
+  margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+
+}
+
+
+
+</style>
